4094c0ef952daf30f3a1195bbd19b832074dc89f
Chin Teng Wei <chime97@oum.edu.my>
2025-08-23 14:03:37 +0800

    Changed seed scriptls location

--- backend/script/seedAdmin.js
+++ backend/script/seedAdmin.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import User from "./models/User.js"; // adjust path
+import User from "../models/User.js"; // adjust path
 
 import dotenv from "dotenv";
 dotenv.config();
